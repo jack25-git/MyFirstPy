a474b4f5608daa43f6cec3db1e2d4cc5be094dbc
jackwu <573016273@qq.com>
2025-04-11 22:40:34 +0800

    修正调用ds的脚本

--- text_ds.py
+++ text_ds.py
@@ -2,12 +2,12 @@ from http.client import responses
 
 import requests
 
-prompt="大语言模型是什么"
+prompt=("请用小学生能听懂的语言解释为什么太阳会下山")
 
 response= requests.post(
     url="http://127.0.0.1:11434/api/generate",
     json={
-        "model":"deepseek-r1:1.5b  ",
+        "model":"deepseek-r1:1.5b",
         "prompt":prompt,
         "stream":False
     }
